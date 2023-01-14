ab141e54c98ecf111b9e7cf368baaab7ae605219
Ville Vartia <ville.v.vartia@utu.fi>
2023-01-14 14:42:30 +0200

    created a function to retrieve data from HSL and store it in a variable

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const axios = require("axios"); //to get data from api endpoint
+const needle = require("needle"); //to get data from api endpoint
 const express = require('express'); //to make an express server
-const { parse } = require("csv-parse"); //to parse csv data
-const fs = require("fs"); //using file system to create a readable stream of data
+const csv = require("csv-parser"); //to parse csv data
+//const fs = require("fs"); //using file system to create a readable stream of data
+
 
 //const dataBaseURL = `dev.hsl.fi/citybikes/od-trips-${year}/${year}-${month}.csv`
 let year = "2021";
@@ -9,13 +10,42 @@ let month ="05";
 
 const dataURL = `https://infopalvelut.storage.hsldev.com/citybikes/od-trips-${year}/${year}-${month}.csv`
 
+const sampleinputPath ="./sampledata/samplejourneys.csv";
+const journeysList = [];
 async function retrieveCityBikeDataFrom(dataURL) {
     //fecthes csv data from the given URL
+    
+    /*
+    data looks like this:
+    {
+        Departure: '2021-05-31T23:05:10',
+        Return: '2021-05-31T23:10:25',
+        Departurestationid: '270',
+        Departurestationname: 'Tulisuontie',
+        Returnstationid: '262',
+        Returnstationname: 'Siilitie (M)',
+        Covereddistancem: '1235',
+        Durationsec: '311'
+      } */
     try {
-       await axios.get(dataURL).then(res =>{
-            console.log(res.status)
-        })
+        needle
+            .get(dataURL)
+            .pipe(csv({
+                mapHeaders: ({ header }) => header.replace(/\s|\(|\)|\./g, '') //get rid off whitespace and other stuff we don't want on headers
+              }))
+            .on('data', (row) => {
+                if(row.Durationsec >= 10 && row.Covereddistancem > 10){ //only add journeys that meet the criteria we have determined
+                    journeysList.push(row)
+                }
+            })
+            .on("done", (err) => {
+                if (err) console.log("An error has occurred");
+                else console.log(`Done. JourneysList has ${journeysList.length} items`); 
+                });
+        
     } catch (error) {
-        console.log("Error in function retrieveCityBikeDataFrom(url) "+error)
+        console.log('error in retrieveCityBikeDataFrom(url)'+error);
     }
 }
+
+retrieveCityBikeDataFrom(dataURL);
