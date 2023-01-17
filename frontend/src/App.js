30006617cf7463907793d6458a168c4b65fa2742
Ville Vartia <ville.v.vartia@utu.fi>
2023-01-17 14:01:17 +0200

    using react router for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
-import "./App.css"
 import React from 'react'
+import "./App.css"
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import JourneyListView from "./components/JourneyListView";
+import StationListView from "./components/StationListView";
+import SingleStationView from "./components/SingleStationView";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 
 export default function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <div className='content'>
+            <Routes>
+              <Route path='/' element ={<Home/>}/>
+              <Route path='/journeylist' element = {<JourneyListView/>}/>
+              <Route path='/stationlist' element = {<StationListView/>}/>
+              <Route path='/singlestation' element = {<SingleStationView/>}/>
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    
   )
 }
 
