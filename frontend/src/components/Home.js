e9b4b51b0faa50e06ea0b8490f7421648ce10390
Ville Vartia <ville.v.vartia@utu.fi>
2023-01-17 15:14:49 +0200

    background image for homeapage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import "./Home.css"
 
 export default function Home() {
   return (
-    <div>Home</div>
+      <>
+        <div className='hero-image'>
+        </div>
+        <div className='hero-text'>
+          <h1 className='h1'>Helsinki Citybikes App</h1>
+         </div>
+      </>
+      
+      
+    
+    
   )
 }
