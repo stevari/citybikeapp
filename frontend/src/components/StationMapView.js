40e60d14855a066356c29be1acc556eea5bc2d84
Ville Vartia <ville.v.vartia@utu.fi>
2023-01-22 12:07:51 +0200

    integrated OpenStreetMap to this application using react-leaflet

--- frontend/src/components/StationMapView.js
+++ frontend/src/components/StationMapView.js
@@ -1,16 +1,23 @@
 import React from 'react'
-
-export default function SingleStationView(props) {
-    /* 
-    Advanced station view component that displays usefull info about the station (given as props) like:
-    -Station name
-    -Address
-    -Total number of journeys starting from the station
-    -Total number of journeys ending at the station
-    */
+import { MapContainer, TileLayer, useMap,Marker,Popup } from 'react-leaflet';
+import "../App.css"
+export default function StationMapView() {
+   
   return (
-    <>
-    SingleStationView
-    </>
+    <div className='leaflet-container'>
+    <MapContainer center={[60.1699, 24.9384]} zoom={13} scrollWheelZoom={true} >
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[60.1699, 24.9384]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+    </div>
+    
+    
   )
 }
