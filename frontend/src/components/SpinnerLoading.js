3427317710a29db11ef7528b920a31098a21e837
Ville Vartia <ville.v.vartia@utu.fi>
2023-01-26 10:54:31 +0200

    spinner to the center of the screen

--- frontend/src/components/SpinnerLoading.js
+++ frontend/src/components/SpinnerLoading.js
@@ -2,7 +2,12 @@ import Spinner from 'react-bootstrap/Spinner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function SpinnerLoading() {
   return (
-    <Spinner animation="border" role="status" style={{color:"orange"}}>
+    <Spinner animation="border" role="status" 
+    style={{
+    color:"orange", 
+    position: "fixed", //cetner of the screen
+    top: "50%",
+    left: "50%"}}>
       <span className="visually-hidden">Loading...</span>
     </Spinner>
   );
